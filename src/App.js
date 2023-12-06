433c538228226fbe2e59b89f4153a2e3eea4e2b3
yainage90 <yainage90@gmail.com>
2023-12-06 20:43:57 +0900

    react-native-paper app registry 등록

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react';
 import { ReadProvider } from './contexts/Read';
 import { SearchProvider } from './contexts/Search';
 import { init_bible_krv, read_bible_krv_by_chapter_idx } from './utils/db';
+import { name as appName } from './app.json';
+import { AppRegistry } from 'react-native';
 import {
   MD3LightTheme as DefaultTheme,
   PaperProvider,
 } from 'react-native-paper';
 import Navigation from './navigations';
+import 'react-native-gesture-handler';
 
 export default App = () => {
   useEffect(() => {
@@ -22,3 +25,5 @@ export default App = () => {
     </PaperProvider>
   );
 };
+
+AppRegistry.registerComponent(appName, () => App);
